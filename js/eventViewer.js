1810a32fe5f22e5cf6f5216664367b9ad5b5c624
r1k <r.i.kelsey@gmail.com>
2014-09-02 17:00:30 +0100

    Add Rickshaw

--- js/eventViewer.js
+++ js/eventViewer.js
@@ -3,12 +3,37 @@ var port;
 var serverFound = true;
 
 var wsUri = "ws://10.20.9.1:8889";
-var bitrateLookup = {};
 
 var msg_handler_list = [];
 var msgCount = 0;
 
 var websocket;
+
+//Rickshaw stuff
+var palette = new Rickshaw.Color.Palette( { scheme: 'classic9' } );
+var graph;
+var messages = [
+      "Changed home page welcome message",
+      "Minified JS and CSS",
+      "Changed button color from blue to green",
+      "Refactored SQL query to use indexed columns",
+      "Added additional logging for debugging",
+      "Fixed typo",
+      "Rewrite conditional logic for clarity",
+      "Added documentation for new methods"
+    ];
+
+//Data store
+var seriesData = [ new Array(0) ];
+var series = [
+		{
+			color: palette.color(),
+			data: seriesData[0],
+			name: '0',
+      pid: 0,
+      updated:false
+		}
+	]
 
 var QueryString = function ()
 {
@@ -67,19 +92,61 @@ var prettifyBitrate = function ( br )
 var updateDisplay = function ()
 {
   var tableString = ""
-  for (pid in bitrateLookup)
+  var totalBitrate = 0;
+  var i;
+  for (i = 0; i < series.length; i++)
   {
-    var line = "<tr><td>" + pid + "</td><td>" + prettifyBitrate(bitrateLookup[pid]) + "</td></tr>"
+    var br = series[i].data[series[i].data.length - 1].y;
+    totalBitrate += br;
+    var line = "<tr><td>" + series[i].name + "</td><td>" + prettifyBitrate(br) + "</td></tr>"
 
     tableString += line;
+
   }
+
+  var line = "<tr><td>Total</td><td>" + prettifyBitrate(totalBitrate) + "</td></tr>"
+  tableString += line;
 
   $('#bitrateTableBody').html(tableString);
 }
 
-var new_bitrate_arrived = function(pid, bitrate)
+var new_bitrate_arrived = function(pid, bitrate, time)
 {
-  bitrateLookup[pid] = bitrate;
+  var i;
+  var found = false;
+  for(i = 0; i < series.length && pid >= series[i].pid; i++)
+  {
+    if (pid === series[i].pid)
+    {
+      series[i].data.push({
+        x:time,
+        y:bitrate
+      });
+      series[i].updated = true;
+      found = true;
+    }
+  }
+
+  if (!found)
+  {
+    var j;
+    // insert new series into array
+    seriesData.splice(i,0, [])
+    for(j = 0; j < seriesData[0].length; j++)
+    {
+      seriesData[i].push({x:seriesData[0][0].x, y:0});
+    }
+    seriesData[i][seriesData[i].length - 1].y = bitrate;
+
+    series.splice(i, 0,
+                  {
+                    color: palette.color(),
+                    data: seriesData[i],
+                    name: pid.toString(),
+                    pid: pid,
+                    updated:true
+                  });
+  }
 }
 
 var bitrate_event_handler = function(msg)
@@ -94,20 +161,39 @@ var bitrate_event_handler = function(msg)
 
 var bitrate_list_handler = function(msg)
 {
-  bitrateLookup = {};
   var numBitrates = msg.bitrates.length;
   var i = 0;
+
+  var timeBase = Math.floor(new Date().getTime() / 1000);
 
   for(i = 0; i < numBitrates; i++)
   {
     var pid = parseInt(msg.bitrates[i].pid);
-    msg.bitrates[i].pid = pid;
     var br = parseInt(msg.bitrates[i].bitrate);
-    msg.bitrates[i].bitrate = br;
 
-    new_bitrate_arrived(pid, br);
+    new_bitrate_arrived(pid, br, timeBase);
   }
 
+  // Go round all the series that weren't updated and set a 0 bitrate
+  for(i = 0; i < series.length; i++)
+  {
+    if (!series[i].updated)
+    {
+      series[i].data.push({
+        x:timeBase,
+        y:0
+      });
+    }
+
+    series[i].updated = false;
+  }
+
+  if (seriesData.length > 100)
+  {
+    seriesData.forEach( function(series) {
+			series.shift();
+	  });
+  }
   return msg;
 }
 
@@ -159,8 +245,6 @@ function onClose(evt)
 
 function onMessage(evt)
 {
-  writeToScreen('RESPONSE: ' + evt.data);
-
   obj = JSON && JSON.parse(evt.data) || $.parseJSON(evt.data);
 
   msg = filterMessages(obj);
@@ -168,7 +252,7 @@ function onMessage(evt)
   if (msg)
   {
     msgCount += 1;
-    if (msgCount > 1000)
+    if (msgCount > 25)
     {
       websocket.close();
     }
@@ -235,8 +319,8 @@ var main = function() {
   var server_details = document.getElementById("server");
   server_details.innerHTML += " " + server_address + " " + port;
 
-  var smoothie = new SmoothieChart();
-  smoothie.streamTo(document.getElementById("mycanvas"));
+  //var smoothie = new SmoothieChart();
+  //smoothie.streamTo(document.getElementById("mycanvas"));
 
   $('#setServer').click(function ()
   {
@@ -253,11 +337,154 @@ var main = function() {
       console.log(wsUri);
 
       msgCount = 0;
+      seriesData = [ new Array(0) ];
+      series = [
+                {
+                  color: palette.color(),
+                  data: seriesData[0],
+                  name: '0',
+                  pid: 0,
+                  updated:false
+                }
+              ]
 
       testWebSocket();
+
   });
 
   window.onbeforeunload = closingCode;
+
+
+
+
+  // Rickshaw code below
+
+
+
+
+
+
+
+// instantiate our graph!
+  var create_graph = function()
+  {
+
+    graph = new Rickshaw.Graph( {
+      element: document.getElementById("chart"),
+      width: 720,
+      height: 400,
+      renderer: 'area',
+      stroke: true,
+      preserve: true,
+      series: series
+    } );
+
+    graph.render();
+
+    var preview = new Rickshaw.Graph.RangeSlider( {
+      graph: graph,
+      element: document.getElementById('preview'),
+    } );
+
+    var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+      graph: graph,
+      xFormatter: function(x) {
+        return new Date(x * 1000).toString();
+      }
+    } );
+
+    var annotator = new Rickshaw.Graph.Annotate( {
+      graph: graph,
+      element: document.getElementById('timeline')
+    } );
+
+    var legend = new Rickshaw.Graph.Legend( {
+      graph: graph,
+      element: document.getElementById('legend')
+
+    } );
+
+    var shelving = new Rickshaw.Graph.Behavior.Series.Toggle( {
+      graph: graph,
+      legend: legend
+    } );
+
+    var order = new Rickshaw.Graph.Behavior.Series.Order( {
+      graph: graph,
+      legend: legend
+    } );
+
+    var highlighter = new Rickshaw.Graph.Behavior.Series.Highlight( {
+      graph: graph,
+      legend: legend
+    } );
+
+    var smoother = new Rickshaw.Graph.Smoother( {
+      graph: graph,
+      element: document.querySelector('#smoother')
+    } );
+
+    var ticksTreatment = 'glow';
+
+    var xAxis = new Rickshaw.Graph.Axis.Time( {
+      graph: graph,
+      ticksTreatment: ticksTreatment,
+      timeFixture: new Rickshaw.Fixtures.Time.Local()
+    } );
+
+    xAxis.render();
+
+    var yAxis = new Rickshaw.Graph.Axis.Y( {
+      graph: graph,
+      tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+      ticksTreatment: ticksTreatment
+    } );
+
+    yAxis.render();
+
+
+    var controls = new RenderControls( {
+      element: document.querySelector('#side_panel'),
+      graph: graph
+    } );
+
+    // add some data every so often
+    function addAnnotation(force) {
+      if (messages.length > 0 && (force || Math.random() >= 0.95)) {
+        annotator.add(seriesData[2][seriesData[2].length-1].x, messages.shift());
+        annotator.update();
+      }
+    }
+
+    addAnnotation(true);
+    setTimeout( function() { setInterval( addAnnotation, 6000 ) }, 6000 );
+
+    var previewXAxis = new Rickshaw.Graph.Axis.Time({
+      graph: preview.previews[0],
+      timeFixture: new Rickshaw.Fixtures.Time.Local(),
+      ticksTreatment: ticksTreatment
+    });
+
+    previewXAxis.render();
+
+  }
+
+  setInterval( function() {
+    if (graph===undefined && seriesData[0].length > 20)
+    {
+      create_graph();
+    }
+    else
+    {
+      graph && graph.update();
+    }
+
+  }, 500 );
+
+
+
+
+
 
 }
 
